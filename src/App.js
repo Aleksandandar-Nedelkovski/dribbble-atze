02030e09387d71ed7b5b44e64b81e39a381c55b8
Aleksandar Nedelkovski <aleksn.dev@gmail.com>
2023-08-28 19:47:19 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Button from "./lib/components/Button";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dribble</h1>
+      <Button label="Hello Dribbble!!!" />
+    </div>
+  );
+}
