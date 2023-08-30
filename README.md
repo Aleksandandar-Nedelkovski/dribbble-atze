5888cde0e903a738dc76b63484c7b93aee0bf340
Aleksandar Nedelkovski <aleksn.dev@gmail.com>
2023-08-30 04:04:28 +0000

    update README.md

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# dribbble-atze
-Created with CodeSandbox
+# dribbble-atze 
+
+### Week 3 - Design System #1 of Scaling Design Systems Dribbble 
+
+### Install in your project
+```
+npm i dribbble-atze
+```
+
+### Usage
+#### Input Component
+```
+<Input type={"text"} id="fname" name="fname" placeholder="First Name" />
+```
+
+#### Button Component
+```
+<Button label="Sign Up" />
+```
--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
-import Header from "./Header";
-import Button from "./lib/components/Button";
-import Input from "./lib/components/Input";
+
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <Header />
-     
-
-  <main className="px-3">
-    <h1>Hello Dribble</h1>
-    <p className="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
-  </main>
-
-  <footer className="mt-auto text-white-50">
-    <p>Placeholder</p>
-      </footer>
-      <form>
-        <label for="fname">First Name</label>
-        <Input type={'text'} id="fname" name="fname" placeholder="First Name" />
-        <label for="lname">Last Name</label>
-        <Input type={'text'} id="fname" name="lname" placeholder="Last Name" />
-        <label for="lname">Password</label>
-        <Input type={'text'} id="fname" name="lname" placeholder="Password" />
-        <Button label="Sign Up" />
-      </form>
+      <main className="px-3">
+        <h1>Hello Dribble</h1>
+        <p className="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+      </main>
     </div>
   );
 }
