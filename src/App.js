d2be3a1e651b221b312ada87da7fab3f4b57c366
Aleksandar Nedelkovski <aleksn.dev@gmail.com>
2023-08-30 04:08:58 +0000

    update landing page

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ export default function App() {
   return (
     <div className="App">
       <main className="px-3">
-        <h1>Hello Dribble</h1>
-        <p className="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+        <h1>Week 3 - Design System #1</h1>
+        <p className="lead">Scaling Design Systems</p>
       </main>
     </div>
   );
